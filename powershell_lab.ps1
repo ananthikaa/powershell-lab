3936e39aaf1263256e906d9f6d103036ea201b64
ananthikaa <157907438+ananthikaa@users.noreply.github.com>
2025-06-03 20:08:12 +0530

    Add PowerShell lab script

---
+++ powershell_lab.ps1
@@ -0,0 +1,23 @@
+# PowerShell Lab Script
+
+# Part 1: Open consoles
+Start-Process powershell
+Start-Process cmd
+
+# Part 2: Run basic commands
+dir
+ping
+cd
+ipconfig
+
+# Part 3: Cmdlets
+Get-Alias dir
+Get-ChildItem
+
+# Part 4: Network diagnostics
+netstat -h
+netstat -r
+netstat -abno
+
+# Part 5: Clear Recycle Bin
+Clear-RecycleBin
